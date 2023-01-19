9e87bf7665b137a26fb04adcdaf00a6b90acfe7a
jin park <jinpark0625@gmail.com>
2023-01-19 01:16:58 -0500

    Add noteComponent & todoComponent

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
-import { PageComponent } from "./components/page.js";
+import { ImageComponent } from "./components/page/item/image.js";
+import { PageComponent } from "./components/page/page.js";
+import { NoteComponent } from "./components/page/item/note.js";
+import { TodoComponent } from "./components/page/item/todo.js";
 
 class App {
   private readonly page: PageComponent;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
+
+    const image = new ImageComponent(
+      "Image Title",
+      "https://picsum.photos/600/300"
+    );
+    image.attachTo(appRoot, "beforeend");
+
+    const note = new NoteComponent("Note Title", "Note Body");
+    note.attachTo(appRoot, "beforeend");
+
+    const todo = new TodoComponent("TodoTitle", "Todo Body");
+    todo.attachTo(appRoot, "beforeend");
   }
 }
 
